887eb99a58fbc1d8e9f475db81518a3414226253
Lucas Mulling <lucas.mulling@suse.com>
2025-01-16 16:23:15 -0300

    More correctly guess buildroot and pacdir
    Signed-off-by: Lucas Mulling <lucas.mulling@suse.com>

--- osc-pod.py
+++ osc-pod.py
@@ -1,5 +1,7 @@
 import os
 import platform
+import getpass
+
 from osc import cmdln
 from osc import core
 from osc import oscerr
@@ -13,6 +15,42 @@ repo2image = {
 arch2arch = {
     'x86_64': 'amd64'
 }
+
+def get_buildroot(apihost, project, package, repo, arch, vm_type, user=None):
+    user = user or ""
+    dash_user = f"-{user:s}" if user else ""
+    buildroot = core.conf.config["build-root"] % {
+        'apihost': apihost,
+        'project': project,
+        'package': package,
+        'repo': repo,
+        'arch': arch,
+        "user": user,
+        "dash_user": dash_user,
+    }
+
+    # Taken from osc/build.py
+    if vm_type != 'lxc' and vm_type != 'nspawn':
+        buildroot = os.path.join(buildroot, '.mount')
+
+    return buildroot
+
+def get_pacdir(buildroot:str, arch: str) -> str:
+    pacdir = os.path.join(buildroot, '.build.packages')
+    if os.path.islink(pacdir):
+        pacdir = os.readlink(pacdir)
+        pacdir = os.path.join(buildroot, pacdir)
+
+    return os.path.join(pacdir, 'RPMS', arch)
+
+def store_read_last_buildroot() -> list[str]:
+    last_broot = core.store_read_last_buildroot('.')
+
+    if last_broot:
+        return last_broot
+    else:
+        raise oscerr.OscBaseError("Could not find package buildroot")
+
 
 @cmdln.option('-w', '--workdir', action='store_true',
               help='use volume as the work dir')
@@ -39,20 +77,14 @@ def do_pod(self, subcmd, opts, *args):
     # TODO: check for --vm-type=kvm|qemu
     # TODO: check if arch matches for the cases above
 
-    repo: str = ''
-    arch: str = ''
     image: str = ''
     target: str = ''
     entrypoint: str = ''
 
-    pkg = core.store_read_package('.')
+    package = core.store_read_package('.')
+    project = core.store_read_project('.')
 
-    last_broot = core.store_read_last_buildroot('.')
-
-    if last_broot:
-        repo, arch, _ = last_broot
-    else:
-        raise oscerr.OscBaseError("Could not find package buildroot")
+    repo, arch, runner = store_read_last_buildroot()
 
     if args and len(args) > 1:
         raise oscerr.WrongArgs("Too many images!")
@@ -62,12 +94,11 @@ def do_pod(self, subcmd, opts, *args):
         # FIXME:
         image = repo2image[repo]
 
-    build_root_dict = { 'dash_user': '', 'repo': repo, 'arch': arch, }
-    build_root = core.conf.config.build_root % build_root_dict
-    rpms = f'{build_root}/home/abuild/rpmbuild/RPMS/{arch}'
+    user = getpass.getuser() if runner in ['podman', 'kvm', 'qemu'] else None
+    buildroot = get_buildroot('', project, package,
+                              repo, arch, runner, user)
 
-    runner = opts.runner
-    volume = opts.volume
+    pacdir = get_pacdir(buildroot, arch)
 
     # Skip pasing `--platform` if the last build was the same target as us,
     # this makes podman start faster
@@ -76,13 +107,16 @@ def do_pod(self, subcmd, opts, *args):
     elif opts.platform:
         target = f"--platform {opts.platform}"
 
+    runner = opts.runner
+    volume = opts.volume
+
     if opts.workdir:
         entrypoint = f'-w {volume}'
 
     if verbose:
-        print(f'Running {runner} image {image} with {pkg} rpms in {volume}')
+        print(f'Running {runner} image {image} with {project}/{package} rpms in {volume}')
 
-    cmd = (f'{runner} run --rm -it -v={rpms}:{volume} {target} {entrypoint} {image}')
+    cmd = (f'{runner} run --rm -it -v={pacdir}:{volume} {target} {entrypoint} {image}')
 
     if debug:
         print(cmd)
